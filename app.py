fd41e097ce6352fbf9d3710186f7214558e3be8c
bomtouinn <tom.bouin@etu.univ-littoral.fr>
2025-03-12 14:19:08 +0100

    Importation de Flask/render_template.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -8,7 +9,7 @@ def home():
 
 @app.route('/about')
 def about():
-    return "À propos de moi : Je m'appelle Tom Bouin et Je suis en train d'apprendre à développer des applications web avec Flask !"
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
