58143a78186ceb99a04ba32b0afb1c1b54790964
bomtouinn <tom.bouin@etu.univ-littoral.fr>
2025-03-12 14:00:29 +0100

    Ajout de la section 'A propos'.

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def home():
 
 @app.route('/about')
 def about():
-    return "À propos de moi : Je suis en train d'apprendre à développer des applications web avec Flask !"
+    return "À propos de moi : Je m'appelle Tom Bouin et Je suis en train d'apprendre à développer des applications web avec Flask !"
 
 if __name__ == '__main__':
     app.run(debug=True)
